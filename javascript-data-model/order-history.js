8a272cc332fbb26e6e84fbdb3b180d226751d30d
moamel97 <muamal1997@gmail.com>
2021-10-12 13:04:07 -0700

    completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,70 @@
+var orderHistory = [
+
+  // Repeated Info
+
+  {
+    shipTo: 'SHIP TO',
+    nameOfThePerson: 'JS Masher',
+    orderDetail: 'Order Details',
+    invoice: 'Invoice',
+    buyItAgain: 'Buy it again',
+    viewYourItem: 'View your item',
+    writeReview: 'Write a product review'
+  },
+  // Reused Info
+  {
+    orderPlaced: 'ORDER PLACED August 4, 2020',
+    totalPriceAfterTax: 34.00,
+    orderNumber: 'ORDER # 114-3941689-87772232',
+    deliveryDate: 'Delivered Aug 8, 2020',
+    image: './image1',
+    orderName: 'JavaScript for impatient programmers',
+    authorName: 'Rauschmayer, Dr. Axel',
+    returnWindow: 'Return window closed on sep 7, 2020',
+    PriceBeforeTax: 31.55
+  },
+  // Second order
+  {
+    orderPlaced: 'ORDER PLACED July 19, 2020',
+    totalPriceAfterTax: 44.53,
+    orderNumber: 'ORDER # 113-9984258',
+    deliveryDate: 'Delivered Jul 20, 2020',
+    image: './image2',
+    orderName: 'The Timeless Way of Building',
+    authorName: 'Alexander , christopher',
+    returnWindow: 'Return window closed on Aug 19, 2020',
+    PriceBeforeTax: 41.33
+  },
+  // Third order
+  {
+    orderPlaced: 'ORDER PLACED July 4, 2020',
+    totalPriceAfterTax: 17.22,
+    orderNumber: 'ORDER # 114-2875557-9059409',
+    deliveryDate: 'Delivered Jul 7, 2020',
+    image: './image3',
+    orderName: 'Gamecube Controller Adapter',
+    returnWindow: 'Return window closed on Aug 5, 2020',
+    PriceBeforeTax: 15.98
+  },
+  // forth order
+  [
+    {
+      orderPlaced: 'ORDER PLACED July 3, 2020',
+      totalPriceAfterTax: 138.93,
+      orderNumber: 'ORDER # 113-2883177-2648248',
+      deliveryDate: 'Delivered Jul 5, 2020',
+      image: './image4',
+      orderName: 'Game Cube Controller',
+      returnWindow: 'Return window colsed Aug 4, 2020',
+      PriceBeforeTax: 94.95,
+      image1: './image5',
+      orderName1: 'The Art of Sql',
+      authorName: 'Faroult, Stephane',
+      returnWindow1: 'Return window closed on Aug 4, 2020'
+    }
+
+  ]
+
+];
+
+console.log('The order of order history:', orderHistory);
