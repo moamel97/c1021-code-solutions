b3229a28e756a9d06bb9be64d5cebfa0c0f41f3d
moamel97 <muamal1997@gmail.com>
2021-10-12 15:26:32 -0700

    completed javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,66 +1,85 @@
 var orderHistory = [
-
-  // Repeated Info
-
   {
     shipTo: 'SHIP TO',
     nameOfThePerson: 'JS Masher',
-    orderDetail: 'Order Details',
-    invoice: 'Invoice',
-    buyItAgain: 'Buy it again',
-    viewYourItem: 'View your item',
-    writeReview: 'Write a product review'
-  },
-  // Reused Info
-  {
     orderPlaced: 'ORDER PLACED August 4, 2020',
     totalPriceAfterTax: 34.00,
     orderNumber: 'ORDER # 114-3941689-87772232',
-    deliveryDate: 'Delivered Aug 8, 2020',
-    image: './image1',
-    orderName: 'JavaScript for impatient programmers',
-    authorName: 'Rauschmayer, Dr. Axel',
-    returnWindow: 'Return window closed on sep 7, 2020',
-    PriceBeforeTax: 31.55
+    item: [
+      {
+        deliveryDate: 'Delivered Aug 8, 2020',
+        orderName: 'JavaScript for impatient programmers',
+        authorName: 'Rauschmayer, Dr. Axel',
+        image: './image1',
+        returnWindow: 'Return window closed on sep 7, 2020',
+        PriceBeforeTax: 31.55
+      }
+    ]
+
   },
   // Second order
   {
+    shipTo: 'SHIP TO',
+    nameOfThePerson: 'JS Masher',
     orderPlaced: 'ORDER PLACED July 19, 2020',
     totalPriceAfterTax: 44.53,
     orderNumber: 'ORDER # 113-9984258',
-    deliveryDate: 'Delivered Jul 20, 2020',
-    image: './image2',
-    orderName: 'The Timeless Way of Building',
-    authorName: 'Alexander , christopher',
-    returnWindow: 'Return window closed on Aug 19, 2020',
-    PriceBeforeTax: 41.33
+    item: [
+      {
+        deliveryDate: 'Delivered Jul 20, 2020',
+        orderName: 'The Timeless Way of Building',
+        authorName: 'Alexander , christopher',
+        returnWindow: 'Return window closed on Aug 19, 2020',
+        image: './image2',
+        PriceBeforeTax: 41.33
+      }
+    ]
+
   },
   // Third order
   {
+    shipTo: 'SHIP TO',
+    nameOfThePerson: 'JS Masher',
     orderPlaced: 'ORDER PLACED July 4, 2020',
     totalPriceAfterTax: 17.22,
     orderNumber: 'ORDER # 114-2875557-9059409',
-    deliveryDate: 'Delivered Jul 7, 2020',
-    image: './image3',
-    orderName: 'Gamecube Controller Adapter',
-    returnWindow: 'Return window closed on Aug 5, 2020',
-    PriceBeforeTax: 15.98
+    item: [
+      {
+        deliveryDate: 'Delivered Jul 7, 2020',
+        returnWindow: 'Return window closed on Aug 5, 2020',
+        orderName: 'Gamecube Controller Adapter',
+        PriceBeforeTax: 15.98,
+        image: './image3'
+      }
+
+    ]
   },
   // forth order
   [
     {
+      shipTo: 'SHIP TO',
+      nameOfThePerson: 'JS Masher',
       orderPlaced: 'ORDER PLACED July 3, 2020',
       totalPriceAfterTax: 138.93,
       orderNumber: 'ORDER # 113-2883177-2648248',
-      deliveryDate: 'Delivered Jul 5, 2020',
-      image: './image4',
-      orderName: 'Game Cube Controller',
-      returnWindow: 'Return window colsed Aug 4, 2020',
-      PriceBeforeTax: 94.95,
-      image1: './image5',
-      orderName1: 'The Art of Sql',
-      authorName: 'Faroult, Stephane',
-      returnWindow1: 'Return window closed on Aug 4, 2020'
+      item: [
+        {
+          deliveryDate: 'Delivered Jul 5, 2020',
+          image: './image4',
+          orderName: 'Game Cube Controller',
+          returnWindow: 'Return window colsed Aug 4, 2020',
+          PriceBeforeTax: 94.95,
+          item: [
+            {
+              orderName1: 'The Art of Sql',
+              image1: './image5',
+              authorName: 'Faroult, Stephane',
+              returnWindow1: 'Return window closed on Aug 4, 2020',
+              PriceBeforeTax: 33.99
+            }
+          ]
+        }
+      ]
     }
 
   ]
