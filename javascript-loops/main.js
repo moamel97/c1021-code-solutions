041424ef073656a96bb7842196fbbb47b0aac020
moamel97 <muamal1997@gmail.com>
2021-10-14 16:32:58 -0700

    completed javascript loops

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -16,3 +16,53 @@ function getNumbersToTen() {
   }
   return numbers;
 }
+
+function getEvenNumbersToTwenty() {
+  var evenNumbers = [];
+  var currentNumber = 2;
+  while (currentNumber <= 20) {
+    evenNumbers.push(currentNumber);
+    currentNumber += 2;
+  }
+  return evenNumbers;
+}
+
+function repeatWord(word, times) {
+  var count = 1;
+  var repeated = '';
+  while (count <= times) {
+    repeated = word;
+    count++;
+  }
+  return repeated;
+}
+
+function logEachCharacter(string) {
+  for (var i = 0; i < string.length; i++) {
+    console.log(string[i]);
+  }
+}
+
+function doubleAll(numbers) {
+  var double = [];
+  for (var i = 0; i < numbers.length; i++) {
+    double.push(numbers[i] * 2);
+  }
+  return double;
+}
+
+function getKeys(object) {
+  var keys = [];
+  for (var key in object) {
+    object.push(key);
+  }
+  return keys;
+}
+
+function getValues(object) {
+  var values = [];
+  for (var key in object) {
+    object.push([key]);
+  }
+  return values;
+}
