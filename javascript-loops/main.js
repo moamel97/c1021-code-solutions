186a6f018802827a8dc11c91b646828353f35614
moamel hashim <muamal1997@gmail.com>
2021-10-14 00:52:32 -0700

    i cant finish loops

---
+++ javascript-loops/main.js
@@ -0,0 +1,18 @@
+/* exported getNumbersToTen,
+            getEvenNumbersToTwenty,
+            repeatWord,
+            logEachCharacter,
+            doubleAll,
+            getKeys,
+            getValues
+ */
+
+function getNumbersToTen() {
+  var numbers = [];
+  var currentNumber = 1;
+  while (numbers[currentNumber]) {
+    numbers += currentNumber;
+    currentNumber++;
+    return numbers
+  }
+}
