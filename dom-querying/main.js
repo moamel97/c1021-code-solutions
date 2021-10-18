1db19ce39dc25d763b38f0d8761f08ab9731abf6
moamel97 <muamal1997@gmail.com>
2021-10-18 12:24:30 -0700

    completed dom querying

---
+++ dom-querying/main.js
@@ -0,0 +1,16 @@
+console.log('hello, word');
+document.querySelector('h1');
+console.log(document.querySelector('h1'));
+console.dir(document.querySelector('h1'));
+
+document.querySelector('#explanation');
+console.log(document.querySelector('#explanation'));
+
+document.querySelector('.hint');
+console.log(document.querySelector('.hint'));
+
+document.querySelectorAll('p');
+console.log(document.querySelectorAll('p'));
+
+document.querySelectorAll('.example-link');
+console.log(document.querySelectorAll('.example-link'));
