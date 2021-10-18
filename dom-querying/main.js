2531e649fa0945dcb6fde013f7ef07ac372ad275
moamel97 <muamal1997@gmail.com>
2021-10-18 14:51:23 -0700

    completed dom quering

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,16 +1,16 @@
 console.log('hello, word');
-document.querySelector('h1');
-console.log(document.querySelector('h1'));
-console.dir(document.querySelector('h1'));
+var $header = document.querySelector('h1');
+console.log('$header', $header);
+console.dir($header);
 
-document.querySelector('#explanation');
-console.log(document.querySelector('#explanation'));
+var $explanation = document.querySelector('#explanation');
+console.log('$explanation', $explanation);
 
-document.querySelector('.hint');
-console.log(document.querySelector('.hint'));
+var $hint = document.querySelector('.hint');
+console.log('$hint', $hint);
 
-document.querySelectorAll('p');
-console.log(document.querySelectorAll('p'));
+var $p = document.querySelectorAll('p');
+console.log('$paragraph', $p);
 
-document.querySelectorAll('.example-link');
-console.log(document.querySelectorAll('.example-link'));
+var $exampleLink = document.querySelectorAll('.example-link');
+console.log('$examplelink', $exampleLink);
