cfbc04b144f20c5c7dce6aee389ef8464a00bf39
moamel hashim <muamal1997@gmail.com>
2021-10-18 00:30:01 -0700

    uncompleted javascript loops practice

---
+++ javascript-loops-practice/countdown.js
@@ -0,0 +1,9 @@
+/* exported countdown */
+
+function countdown(number) {
+  var input = [];
+  for (var i = number; i >= 0; i--) {
+    input.push(i);
+  }
+  return input
+}
---
+++ javascript-loops-practice/sum-all.js
@@ -0,0 +1,9 @@
+/* exported sumAll */
+
+function sumAll(numbers) {
+  var input = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    input += numbers[i];
+  }
+  return input
+}
