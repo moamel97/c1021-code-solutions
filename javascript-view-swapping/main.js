cdbedb1c463dae1b921db498365aa72dea5785a1
moamel97 <muamal1997@gmail.com>
2021-10-22 15:31:00 -0700

    uncompleted view swapping

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,22 @@
+// var $container = document.querySelector('.tab-container');
+// var $tab = document.querySelectorAll('.tab');
+// var $view = document.querySelectorAll('.view');
+// $container.addEventListener('click', switchTabs);
+// console.log($container);
+// console.log($tab);
+// console.log($view);
+// function switchTabs(event) {
+//   if (event.target.matches('.tab') === false) {
+//     return;
+//   }
+//   for (var tabIndex = 0; tabIndex < $tab.length; tabIndex++) {
+//     if ($tab[tabIndex] === event.target) {
+//       $tab[tabIndex].className = 'tab active';
+//     } else {
+//       $tab[tabIndex].className = 'tab';
+//     }
+//   }
+//   var viewData = {
+//   };
+//   console.log(viewData);
+// }
