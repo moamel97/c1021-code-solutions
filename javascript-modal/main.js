60b02843255e240188866a6d4078928946038cad
moamel hashim <muamal1997@gmail.com>
2021-10-23 23:58:30 -0700

    completed javascript modal

---
+++ javascript-modal/main.js
@@ -0,0 +1,30 @@
+var onAndOff = true;
+var $button = document.querySelector('button');
+var $overlay = document.querySelector('div');
+var $positionSurvey = document.querySelector('div.column-hidden')
+var $surveyButton = document.querySelector('.survey-button-styling');
+  console.log($surveyButton);
+  console.log($button);
+  console.log($overlay);
+  // console.log($positionSurvey);
+
+  function survey(event) {
+    if (onAndOff === true) {
+      $button.className = 'button-style';
+      $positionSurvey.className = 'position-survey';
+      $overlay.className = 'overlay';
+      onAndOff = false;
+    }
+ }
+
+ function noSurvey (event) {
+   if (onAndOff === false) {
+     $surveyButton = 'survey-button-styling';
+     $positionSurvey.className = 'column-hidden';
+     $overlay.className = '';
+     onAndOff = true
+   }
+ }
+
+  $button.addEventListener('click', survey);
+   $surveyButton.addEventListener('click', noSurvey)
