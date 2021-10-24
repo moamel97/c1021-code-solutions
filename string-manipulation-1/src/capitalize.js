9dab3c703b3921b9dc235d10d8809f25ec7cd497
moamel97 <muamal1997@gmail.com>
2021-10-23 19:14:43 -0700

    uncompleted string manipulation 1

---
+++ string-manipulation-1/src/capitalize.js
@@ -0,0 +1,9 @@
+/* exported capitalize */
+/* we make a funtion that can access the first word of a string and then capitalize the output of the string.
+we also should use the capitalize method for the first latter of the word */
+
+function capitalize(word) {
+  var firstIndex = '';
+  var upperCase = firstIndex.toUpperCase();
+  return upperCase;
+}
---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,15 @@
+/* exported getFirstChar */
+
+/* the function should have a string with a paramater of a string.
+next step is should stroe strings.
+then it should choose the first latter of a word.
+after it stores and chose the first latter of a string.
+it should give us an output of the first latter of a word.
+[x]creat a fucntion that stroes a string
+[x]give a name to that funtion
+[x]that function should chose the first latter of a word and then return an output.
+*/
+
+function getFirstChar(string) {
+  return string[0];
+}
