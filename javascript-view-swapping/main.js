252f97e096e42123752b856dc854cca22d7f2aea
moamel97 <muamal1997@gmail.com>
2021-10-23 18:08:56 -0700

    completed javascript view swapping

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,22 +1,26 @@
-// var $container = document.querySelector('.tab-container');
-// var $tab = document.querySelectorAll('.tab');
-// var $view = document.querySelectorAll('.view');
-// $container.addEventListener('click', switchTabs);
-// console.log($container);
-// console.log($tab);
-// console.log($view);
-// function switchTabs(event) {
-//   if (event.target.matches('.tab') === false) {
-//     return;
-//   }
-//   for (var tabIndex = 0; tabIndex < $tab.length; tabIndex++) {
-//     if ($tab[tabIndex] === event.target) {
-//       $tab[tabIndex].className = 'tab active';
-//     } else {
-//       $tab[tabIndex].className = 'tab';
-//     }
-//   }
-//   var viewData = {
-//   };
-//   console.log(viewData);
-// }
+var $container = document.querySelector('.tab-container');
+var $tab = document.querySelectorAll('.tab');
+var $view = document.querySelectorAll('.view');
+$container.addEventListener('click', switchTabs);
+
+function switchTabs(event) {
+  if (event.target.matches('.tab') === false) {
+    return;
+  }
+  for (var tabIndex = 0; tabIndex < $tab.length; tabIndex++) {
+    if ($tab[tabIndex] === event.target) {
+      $tab[tabIndex].className = 'tab active';
+    } else {
+      $tab[tabIndex].className = 'tab';
+    }
+  }
+  var viewName = event.target.getAttribute('data-view');
+
+  for (var viewIndex = 0; viewIndex < $view.length; viewIndex++) {
+    if ($view[viewIndex].getAttribute('data-view') !== viewName) {
+      $view[viewIndex].className = 'view hidden';
+    } else {
+      $view[viewIndex].className = 'view';
+    }
+  }
+}
