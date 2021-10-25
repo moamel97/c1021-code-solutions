4329232e14a639cf51e3b82f17cf857a79e7ff5c
moamel hashim <muamal1997@gmail.com>
2021-10-24 17:15:56 -0700

    updated javascript forms

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -1,21 +1,27 @@
 var $input = document.querySelector('input');
 var $textAreae = document.querySelector('textarea');
-
+var $email = document.querySelector('#user-email');
 function handleFocus(event) {
   console.log('focus event was fired');
   console.log(event.target.name);
 }
 $input.addEventListener('focus', handleFocus);
+$textAreae.addEventListener('focus', handleFocus);
+$email.addEventListener('focus', handleFocus);
 
 function handleBlur(event) {
   console.log('blur event was fired');
   console.log(event.target.name);
 }
 $input.addEventListener('blur', handleBlur);
+$textAreae.addEventListener('blur', handleBlur);
+$email.addEventListener('blur', handleBlur);
 
 function handleInput(event) {
   console.log(event.target.name);
   console.log(event.target.value);
 }
 $input.addEventListener('input', handleInput);
+$textAreae.addEventListener('input', handleInput);
+$email.addEventListener('input', handleInput);
 console.log($textAreae);
