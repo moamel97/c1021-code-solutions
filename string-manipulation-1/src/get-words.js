47b6f8cbeeb898b39741d83718c3efecf6e9cc1d
moamel hashim <muamal1997@gmail.com>
2021-10-31 20:01:27 -0700

    completed string manipulation 1

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,1 +1,12 @@
 /* exported getWords */
+/* create a function that creates an array and seprate each word as well as check if the string has a word inside it and if it left empty then return an empty erray
+if not the return the sentance split up */
+
+function getWords(string) {
+  if (string === '') {
+    var array = [];
+    return array;
+  }
+ var splits = string.split(' ');
+return splits;
+}
