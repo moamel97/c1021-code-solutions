c4945248546dc90ff008fe5829b5b9d0e9d30ba5
moamel97 <muamal1997@gmail.com>
2021-11-04 22:53:20 -0700

    still in progress

---
+++ string-manipulation-2/src/truncate.js
@@ -0,0 +1,24 @@
+/* exported truncate */
+/* step 1 create a function that holds 2 parameters the first parameter is a number the second one is a string
+step 2 create a variable that is an empty array[];
+step 3 create a for loop that loops over the length of each word in the index
+step 4 inside the for loop code block use the substring() method to substring the text that are being passed in
+the string parameter then we pass the parameter "number" as an argument to the substring method so that it removes
+latters depending on what number is being inputted then after that we change the text content to equal string[i] + '....'
+that way it displays the dots after the text have been trimmed
+step 5 we push the string at [i] into the array variable.
+step 6 return array
+          !important!
+          console.log everything and debug
+*/
+
+// function truncate(number, string) {
+//   var array = [];
+//   for (var i = 0; i < string.length; i++) {
+//     // string[i].substring(number);
+//     // array.join(string[i]);
+//     array.push(string[i]);
+//   }
+//   console.log(array);
+//   return array + '....';
+// }
