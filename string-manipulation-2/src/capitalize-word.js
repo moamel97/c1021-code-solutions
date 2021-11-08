0cc6fb8b484f72ba87d70edb36dff6ffc92b0a7f
moamel hashim <moamel1997@gmail.com>
2021-11-08 00:03:07 +0000

    completed some

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,15 @@
 /* exported capitalizeWord */
+/* step 1 create a function called capitalizeWord and give it one parameter word
+step 2 create a variable that lower case words
+step 3 create a condition that checks if the lower cased word is strictly equal to javascript
+step 4 return 'JavaScript'
+step 5 return the word and capitalize the first letter of it.
+*/
+
+function capitalizeWord(word) {
+  var lowerCase = word.toLowerCase();
+  if (lowerCase === 'javascript') {
+    return 'JavaScript';
+  }
+  return word[0].toUpperCase() + lowerCase.slice(1);
+}
--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -4,19 +4,16 @@ step 2 create a variable that is an empty array[];
 step 3 create a for loop that loops over the length of each word in the index
 step 4 inside the for loop code block use the substring() method to substring the text that are being passed in
 the string parameter then we pass the parameter "number" as an argument to the substring method so that it removes
-latters depending on what number is being inputted then after that we change the text content to equal string[i] + '....'
+letters depending on what number is being inputted then after that we change the text content to equal string[i] + '....'
 that way it displays the dots after the text have been trimmed
 step 5 we push the string at [i] into the array variable.
 step 6 return array
+note <bold> since the substring method acts like a for loop there is no need to use a for loop.. so make sure to delete the for loop
+whenever u start working on the challenge again. thank you for coming to my ted talk.
           !important!
           console.log everything and debug
 */
 
 function truncate(number, string) {
-  var array = [];
-  for (var i = 0; i < string.length; i++) {
-    string.substring(number);
-    array.push(string[i]);
-  }
-  return array.join() + '....';
+  return string.substring(0, number) + '...';
 }
