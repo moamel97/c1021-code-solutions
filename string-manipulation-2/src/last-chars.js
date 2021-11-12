98df116bf3042b3eba5b65616dfc3fe77e91b5fb
moamel hashim <moamel1997@gmail.com>
2021-11-12 07:35:04 +0000

    completed some problems

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,11 @@
 /* exported lastChars */
+function lastChars(length, string) {
+  var str = '';
+  if (string.length < length) {
+    return string;
+  }
+  for (var i = string.length - length; i < string.length; i++) {
+    str += string[i];
+  }
+  return str;
+}
