05c6d38dcb12b800e5afd884a0dfe492ef672c9e
moamel hashim <muamal1997@gmail.com>
2021-12-09 16:38:53 -0800

    updated compact

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -15,7 +15,7 @@ step 7 return the newArray;
 function compact(array) {
   const newArray = [];
   for (let i = 0; i < array.length; i++) {
-    if ((array[i] !== false) && (array[i] !== undefined) && (array[i] !== null) && (array[i]) && (array[i] !== 0) && (array[i] !== '')) {
+    if (array[i]) {
       newArray.push(array[i]);
     }
   }
